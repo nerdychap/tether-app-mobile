c24066c86513483e4ad4146f807169c293e04b58
Siyabonga Hlengwa <siyabongahlengwa@Siyabongas-MacBook-Pro.local>
2024-11-20 13:06:25 +0200

    adding space

--- components/screens/OrderBook.tsx
+++ components/screens/OrderBook.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const OrderBook = () => {
   return (
     <View style={styles.container}>
-      <Text>OrderBook</Text>
+      <Text>Order Book</Text>
     </View>
   );
 };
